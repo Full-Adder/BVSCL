d1439e0f0c7f4f1db1f4ffa518a9ec4a575794a6
full_adder <000@000.com>
2024-10-29 18:22:32 +0800

    updata

--- Readme.md
+++ Readme.md
@@ -4,3 +4,17 @@ Offical implemention of the paper BVSCL: A Biologically-inspired Visual Saliency
 
 ![alt text](img/image.png)
 
+## 准备数据
+视听数据集：
+http://cvsp.cs.ntua.gr/research/stavis/
+视觉数据集：
+https://www.crcv.ucf.edu/data/UCF101.php
+需要重构一下目录结构
+
+## 
+```shell
+python train.py
+```
+
+
+
--- main.py
+++ main.py
@@ -31,10 +31,9 @@ if __name__ == "__main__":
     #  target['salmap'] = torch.ones(8, 1, 112, 112)
     print(f"use {platform.system()}, torch {torch.__version__}, torchvision {torchvision.__version__}, torchaudio {torchaudio.__version__}")
     for task in task_list:
-        data_loader = get_dataloader(root=saliency_path, mode='val', task=task)
+        data_loader,_ = get_dataloader(root=saliency_path, mode='val', task=task)
         print(f"{task= }, {len(data_loader)= }")
-        for i,v in enumerate(data_loader[0]):
-            print(v)
+        for i,(data, target, vail) in enumerate(data_loader[0]):
             print(i, data['rgb'].shape, data['audio'].shape, target['salmap'].shape, target['binmap'].shape)
             cc = CC( target['salmap'], target['salmap'])
             nss = NSS( target['salmap'], target['binmap'])
